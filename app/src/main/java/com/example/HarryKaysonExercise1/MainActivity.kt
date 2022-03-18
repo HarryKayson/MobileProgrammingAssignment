1adf3859939061102eddb5f7f30c983a550ceef9
HarryKayson <77840620+HarryKayson@users.noreply.github.com>
2022-03-18 15:45:49 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/HarryKaysonExercise1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.HarryKaysonExercise1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: View) {}
+    fun countUp(view: View) {}
+}
